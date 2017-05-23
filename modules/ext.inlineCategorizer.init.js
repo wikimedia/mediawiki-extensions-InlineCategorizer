7ecad99a037b7be21b0ed7884110f9b546756bae
Timo Tijhof <krinklemail@gmail.com>
2017-05-23 21:05:12 +0200

    init: Avoid using mw.page which may not exist
    This object is owned by 'mediawiki.page.startup' in core. It
    is not used anywhere in any extension (including this one) and
    mostly just not needed to be exposed.
    
    A dependency on that module was also missing, making it break
    under certain race conditions.
    
    Change-Id: Ie5e411cdf6509d25df8f0f1d91092deeb10189b2

--- modules/ext.inlineCategorizer.init.js
+++ modules/ext.inlineCategorizer.init.js
@@ -1,9 +1,6 @@
-jQuery( document ).ready( function () {
-	/**
-	* Initialize an instance of InlineCategorizer into mw.page
-	*/
-	mw.page.inlineCategorizer = new mw.InlineCategorizer();
+jQuery( function () {
+	var categorizer = new mw.InlineCategorizer();
 	// Separate function for call to prevent jQuery
 	// from executing it in the document context.
-	mw.page.inlineCategorizer.setup();
-});
+	categorizer.setup();
+} );
