5a2a3c9fe1ea49713b13d2e1341241f6711ba678
Fomafix <fomafix@googlemail.com>
2024-12-14 16:42:07 +0000

    Use service MainConfig instead of global variable
    Change-Id: I14052c60036d501308f86c8ac775a30cb471f3f6

--- InlineCategorizerHooks.php
+++ InlineCategorizerHooks.php
@@ -10,17 +10,25 @@ class InlineCategorizerHooks implements
 	\MediaWiki\Hook\BeforePageDisplayHook
 {
 
+	private Config $config;
+
+	public function __construct(
+		Config $config
+	) {
+		$this->config = $config;
+	}
+
 	/**
 	 * @param OutputPage $out
 	 * @param Skin $skin
 	 */
 	public function onBeforePageDisplay( $out, $skin ): void {
-		global $wgInlineCategorizerNamespaces;
+		$namespaces = $this->config->get( 'InlineCategorizerNamespaces' );
 
 		// Only load if there are no restrictions, or if the current namespace
 		// is in the array.
-		if ( count( $wgInlineCategorizerNamespaces ) === 0
-			|| in_array( $out->getTitle()->getNamespace(), $wgInlineCategorizerNamespaces )
+		if ( count( $namespaces ) === 0
+			|| in_array( $out->getTitle()->getNamespace(), $namespaces )
 		) {
 			$out->addModules( 'ext.inlineCategorizer.core' );
 		}
